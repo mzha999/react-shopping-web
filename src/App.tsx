f864c470a7508c1a7e3c7215646ffc228759cd24
Kenny Zhao <53667317+mzha999@users.noreply.github.com>
2021-10-31 20:16:03 +1300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer } from "@mui/material";
+import { LinearProgress } from "@mui/material";
+import { Grid } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { Badge } from "@mui/material";
+import { Wrapper, StyledButton } from "./App.styles";
+import Item from "./components/Item";
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  const getTotalItems = (items: CartItemType[]) => items.reduce((ack: number, items)=> ack+items.amount, 0);
+
+  const handleAddToCart = (clickedItem: CartItemType[]) => null;
+  const handleRemoveFromCart = () =>{
+    console.log('hello')
+  };
+
+  isLoading && <LinearProgress />;
+  error && <div>Errors...</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className="App">
+      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+        Cart here
+      </Drawer>
+      <StyledButton onClick={() => setOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleRemoveFromCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
